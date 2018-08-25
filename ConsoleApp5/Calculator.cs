b3bbd19f9f4a92e4e25371d4d7f062f1c8e8079c
OlgaKulina <kulinaolg@gmail.com>
2018-08-25 17:12:03 +0300

    Add project files.

---
+++ ConsoleApp5/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Calculator
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Mult(double a, double b)
+        {
+            return a * b;
+        }
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+
+    }
+}
